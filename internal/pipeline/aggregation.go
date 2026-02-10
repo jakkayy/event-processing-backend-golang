2c4ad19c95e1dab5114f3c10daf54b8cccf051a2
jakkayy <niti2042547@gmail.com>
2026-02-11 01:55:36 +0700

    feat: create aggregation processor

---
+++ internal/pipeline/aggregation.go
@@ -0,0 +1,39 @@
+package pipeline
+
+import (
+	"log"
+	"sync"
+
+	"event-processing-backend-golang/internal/domain"
+)
+
+type AggregationProcessor struct {
+	mu       sync.Mutex
+	counters map[string]int
+}
+
+func NewAggregationProcessor() *AggregationProcessor {
+	return &AggregationProcessor{
+		counters: make(map[string]int),
+	}
+}
+
+func (p *AggregationProcessor) Process(e domain.Event) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	p.counters[e.Type]++
+
+	log.Printf("[aggregate] type=%s count=%d\n", e.Type, p.counters[e.Type])
+}
+
+func (p *AggregationProcessor) snapshot() map[string]int {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	result := make(map[string]int)
+	for k, v := range p.counters {
+		result[k] = v
+	}
+	return result
+}
